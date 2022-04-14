7ca7a998723884e7abf617a9db67b4afbdfcea1b
Jaine Kiefer <jainekiefer013@gmail.com>
2022-04-13 21:42:06 -0300

    master

---
+++ velha.js
@@ -0,0 +1,18 @@
+var jogador = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+
+mudarJogador('X');
+
+function escolherQuadrado(id){
+    var quadrado = document.getElementById(id);
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+}
+
+function mudarJogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+}
+
+//AULA 06
